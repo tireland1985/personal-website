4217e5c5e1278479eb7b61d57dad42f7a7b54fb7
tireland1985 <tim.ireland19@my.northampton.ac.uk>
2023-01-13 03:07:01 +0000

    ..

--- conf/database.php
+++
@@ -1,4 +0,0 @@
-<?php
-    require_once 'secrets.php';
-	$pdo = new PDO('mysql:dbname=' . $DB_NAME . ';host=' . $DB_HOST, $DB_USER, $DB_PASSWORD,
-    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
