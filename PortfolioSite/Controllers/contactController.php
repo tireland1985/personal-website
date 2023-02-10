7ca442843c0103030af4e43d709f943ff9a82390
tireland1985 <tim.ireland19@my.northampton.ac.uk>
2023-02-10 02:00:20 +0000

    initial implementation of contact form

--- PortfolioSite/Controllers/contactController.php
+++ PortfolioSite/Controllers/contactController.php
@@ -1,5 +1,8 @@
 <?php
 namespace PortfolioSite\Controllers;
+use \PHPMailer\PHPMailer\PHPMailer;
+use \PHMailer\PHPMailer\Exception;
+
 class contactController{
     public function __construct(){
 
@@ -12,5 +15,48 @@ class contactController{
             'title' => 'Contact',
             'variables' => []
         ];
+    }
+
+    public function formSubmit(){
+
+        $username = $MAILER_USERNAME; //get from secrets file
+        $password = $MAILER_PASSWORD; //get from secrets file
+        $myEmail = $MAILER_EMAIL; //get from secrets file
+
+        require '../../PHPMailer/src/Exception.php';
+        require '../../PHPMailer/src/PHPMailer.php';
+        require '../../PHPMailer/src/SMTP.php';
+
+        if(isset($_POST['submit'])){
+	        $mail = new PHPMailer(true);
+	
+	        $mail->SMTPDebug = 0;
+	
+	        $mail->Host = 'v.je'; //use real domain, not v.je as dev env does not have smtp 
+	        $mail->SMTPAuth = true;
+	        $mail->Username = $username;
+	        $mail->Password = $password;
+	        $mail->SMTPSecure = 'tls';
+	        $mail->Port = '587';
+	
+	        $mail->setFrom($username, 'Mailer');
+	        $mail->addAddress($myEmail);
+	        $mail->addReplyTo($_POST['email'], $_POST['name']);
+	
+	        $mail->isHTML(true);
+	        $mail->Subject = 'Contact request from: ' . $_POST['name'];
+	        $mail->Body = $_POST['message'];
+	
+	        try {
+		        $mail->send();
+		        echo 'Your message has been sent';
+	        }
+	        catch (\Exception $e) {
+		        echo "Message sending failed. Error: {$mail->ErrorInfo}";
+        	}
+        }
+        else {
+	        echo 'There appears to be an issue with the contact form, please try again later';
+        }
     }
 }
---
+++ templates/contact-form.html.php
@@ -0,0 +1,61 @@
+<main>
+        <!-- Material Forms: https://materializecss.com/text-inputs.html viewed: 15/03/2020, some aspects from Assignment 1 modified. -->
+        <div class="row">
+            <div class="contact-form">
+                <div class="info">
+                    <h3>Tim Ireland</h3>
+                    <ul class="ul_form">
+                        <li><i class="white-text fa fa-road"></i>&nbsp;&nbsp;Milton Keynes</li>
+                        <li><i
+                                class="white-text fa fa-envelope"></i>&nbsp;&nbsp;&#116;im&#64;timire&#108;a&#110;&#100;&#46;u&#107;
+                        </li>
+                    </ul>
+                </div>
+                <!-- <script>alert("E-Mails sent using this form are not currently being received. In the mean time, please use the email address listed. Apologies for the inconvenience");</script> -->
+                <div class="row contact-me">
+                    <h3>E-mail Me</h3>
+                    <div class="card horizontal card-opacity center">
+                        <div class="card-stacked">
+                            <!-- form uses validateForm() function to check data inputs to name, email & message fields. -->
+                            <!-- also checks that email address entered appears to be a valid format -->
+                            <form class="col s12" id="contact-me" name="contactForm" onsubmit="return(validateForm());" action="/contact/form" method="post">
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        <i class="material-icons prefix">account_circle</i>
+                                        <input placeholder="Enter your name" id="name" type="text" class="validate white-text" name="name" value="<?php echo isset($_POST['name']) ? $data['name'] : ''; // prevent removing value from the form after failed sending ?>" required oninvalid="this.setCustomValidity('Input your name')" oninput="setCustomValidity('')">
+                                        <label for="name" class="white-text">Name</label>
+                                    </div>
+
+                                </div>
+                                <div class="row">
+                                    <div class="input-field col s6">
+                                        <i class="material-icons prefix">email</i>
+                                        <input type="email" class="validate white-text" id="email" name="email" value="<?php echo isset($_POST['email']) ? $data['email'] : ''; // prevent removing value from the form after failed sending ?>" required>
+                                        <label for="email" class="white-text" data-error="wrong" data-success="right">Email</label>
+                                    </div>
+                                    <div class="input-field col s6">
+                                        <i class="material-icons prefix">call</i>
+                                        <input type="tel" class="validate white-text" id="phone" name="phone" value="<?php echo isset($_POST['phone']) ? $data['phone'] : ''; ?>">
+                                        <label for="phone" class="white-text">Telephone</label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        <i class="material-icons prefix">mode_edit</i>
+                                        <textarea id="textarea2" class="materialize-textarea white-text validate" name="message" data-length="120" required oninvalid="this.setCustomValidity('Input your message')" oninput="setCustomValidity('')"><?php echo isset($_POST['message']) ? $data['message'] : ''; ?></textarea>
+                                        <label for="textarea2" class="white-text">Message</label>
+                                    </div>
+                                </div>
+                                <button type="submit">Submit &nbsp;<i class="material-icons white-text">send</i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s12 m12 center">
+                <span id="date-time"></span>
+            </div>
+        </div>
+    </main>
