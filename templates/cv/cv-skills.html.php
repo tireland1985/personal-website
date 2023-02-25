3896184a2bcb95af717e5bb715b123429efe330f
tireland1985 <tim.ireland19@my.northampton.ac.uk>
2023-02-25 09:25:18 +0000

    static version of skills page, complete with modals- will eventually be converted to dynamic

--- templates/cv/cv-skills.html.php
+++ templates/cv/cv-skills.html.php
@@ -5,14 +5,195 @@
             <div class="col s12 m12 center">
                 <h1>Skills</h1>
                 <h2>Click a skill for details</h2>
-            <a href="#skills-modal-test" class="btn modal-trigger waves-effect waves-light cv-skill" id="">Cisco IOS</a>
+            <a href="#skills-modal-test" class="btn modal-trigger waves-effect waves-light card-opacity-cv" id="">Cisco IOS</a>
 				
-			   <div id="skills-modal-test" class="modal cv-skill">
-                    <div class="modal-content cv-skill white-text">
+			   <div id="skills-modal-test" class="modal card-opacity-cv">
+                    <div class="modal-content card-opacity-cv white-text">
                         <h4>Cisco IOS</h4>
-                        <p>Experience with IOS 15 through simulators, emulators and real equipment.</p>
+                        <p>Experience with IOS 15 through a mix of CML-Personal, Packet Tracer, and cisco hardware.</p>
                     </div>
-                <div class="modal-footer cv-skill">
+                <div class="modal-footer card-opacity-cv">
+                    <a href="#!" class="modal-action modal-close btn blue darken-4 white-text">Close</a>
+                </div>
+            </div>
+			&nbsp; &nbsp;
+			<a href="#skills-modal-test2" class="btn modal-trigger waves-effect waves-light card-opacity-cv" id ="">DHCP</a>
+				
+			   <div id="skills-modal-test2" class="modal card-opacity-cv">
+                    <div class="modal-content card-opacity-cv white-text">
+                        <h4>Dynamic Host Configuration Protocol</h4>
+                        <p>Experience with several DHCP servers, including  Microsoft Windows Server 2012R2, Cisco's EasyDHCP (IOS) and the ISC DHCP Server.</p>
+                    </div>
+                <div class="modal-footer card-opacity-cv">
+                    <a href="#!" class="modal-action modal-close btn blue darken-4 white-text">Close</a>
+                </div>
+            </div>
+						&nbsp; &nbsp;
+			<a href="#skills-modal-test3" class="btn modal-trigger waves-effect waves-light card-opacity-cv" id ="">DNS</a>
+				
+			   <div id="skills-modal-test3" class="modal card-opacity-cv">
+                    <div class="modal-content card-opacity-cv white-text">
+                        <h4>Domain Name Service</h4>
+                        <p>Configured forward and reverse zones with all records on both Windows Server 2012R2 and BIND9. Familiar with third-party DNS services such as those available on Digital Ocean.</p>
+                    </div>
+                <div class="modal-footer card-opacity-cv">
+                    <a href="#!" class="modal-action modal-close btn blue darken-4 white-text">Close</a>
+                </div>
+            </div>
+						&nbsp; &nbsp;
+			<a href="#skills-modal-test4" class="btn modal-trigger waves-effect waves-light card-opacity-cv" id ="">Linux</a>
+				
+			   <div id="skills-modal-test4" class="modal card-opacity-cv">
+                    <div class="modal-content card-opacity-cv white-text">
+                        <h4>Linux</h4>
+                        <p>Comfortable using debian derivatives such as Ubuntu, some experience with Red Hat/ CentOS.</p>
+                    </div>
+                <div class="modal-footer card-opacity-cv">
+                    <a href="#!" class="modal-action modal-close btn blue darken-4 white-text">Close</a>
+                </div>
+            </div>
+						&nbsp; &nbsp;
+			<a href="#skills-modal-test5" class="btn modal-trigger waves-effect waves-light card-opacity-cv" id ="">BIND 9</a>
+				
+			   <div id="skills-modal-test5" class="modal card-opacity-cv">
+                    <div class="modal-content card-opacity-cv white-text">
+                        <h4>Berkley Internet Name Daemon</h4>
+                        <p>Configured a BIND 9 master/slave system serving multiple subdomains/ subnets in a lab environment on two Raspberry Pi's. Also utilised DNS-over-TLS for forwarding requests.</p>
+                    </div>
+                <div class="modal-footer card-opacity-cv">
+                    <a href="#!" class="modal-action modal-close btn blue darken-4 white-text">Close</a>
+                </div>
+            </div>
+						&nbsp; &nbsp;
+			<a href="#skills-modal-test6" class="btn modal-trigger waves-effect waves-light card-opacity-cv" id ="">Microsoft Windows</a>
+				
+			   <div id="skills-modal-test6" class="modal card-opacity-cv">
+                    <div class="modal-content card-opacity-cv white-text">
+                        <h4>Microsoft Windows</h4>
+                        <p>Experience with various client versions of Windows, ranging from XP to 10. Experience with Server Operating Systems: 2008R2 and 2012R2.</p>
+                    </div>
+                <div class="modal-footer card-opacity-cv">
+                    <a href="#!" class="modal-action modal-close btn blue darken-4 white-text">Close</a>
+                </div>
+            </div>
+						&nbsp; &nbsp;
+			<a href="#skills-modal-test7" class="btn modal-trigger waves-effect waves-light card-opacity-cv" id ="">Active Directory</a>
+				
+			   <div id="skills-modal-test7" class="modal card-opacity-cv">
+                    <div class="modal-content card-opacity-cv white-text">
+                        <h4>Microsoft Active Directory</h4>
+                        <p>Experience in a lab environment with basic setup and configuration of Active Directory, including GPO's and an internal CA.</p>
+                    </div>
+                <div class="modal-footer card-opacity-cv">
+                    <a href="#!" class="modal-action modal-close btn blue darken-4 white-text">Close</a>
+                </div>
+            </div>
+						&nbsp; &nbsp;
+			<a href="#skills-modal-test8" class="btn modal-trigger waves-effect waves-light card-opacity-cv" id ="">Multilayer Switching</a>
+				
+			   <div id="skills-modal-test8" class="modal card-opacity-cv">
+                    <div class="modal-content card-opacity-cv white-text">
+                        <h4>Layer 3 or Multilayer Switching</h4>
+                        <p>Mix of Packet Tracer, CML, and hands-on experience on Catalyst 3550 and 3560's - Multiple VLANs with SVIs configured for default gateways, dynamic routing via EIGRP and OSPF.</p>
+                    </div>
+                <div class="modal-footer card-opacity-cv">
+                    <a href="#!" class="modal-action modal-close btn blue darken-4 white-text">Close</a>
+                </div>
+            </div>
+						&nbsp; &nbsp;
+			<a href="#skills-modal-test9" class="btn modal-trigger waves-effect waves-light card-opacity-cv" id ="">Dynamic Routing</a>
+				
+			   <div id="skills-modal-test9" class="modal card-opacity-cv">
+                    <div class="modal-content card-opacity-cv white-text">
+                        <h4>Dynamic Routing</h4>
+                        <p>Experience with EIGRP, both classic and named-mode, OSPF and limited BGP from University assignments which required use of Packet Tracer, real hardware when possible, and in one case CML .</p>
+                    </div>
+                <div class="modal-footer card-opacity-cv">
+                    <a href="#!" class="modal-action modal-close btn blue darken-4 white-text">Close</a>
+                </div>
+            </div>
+			
+						&nbsp; &nbsp;
+			<a href="#skills-modal-test10" class="btn modal-trigger waves-effect waves-light card-opacity-cv" id ="">IPv6</a>
+				
+			   <div id="skills-modal-test10" class="modal card-opacity-cv">
+                    <div class="modal-content card-opacity-cv white-text">
+                        <h4>IPv6</h4>
+                        <p>Knowledge of IPv6 mainly through study, although did successfully configure an ISR 2911 to acquire a dynamic prefix from an ISP(/56) via DHCP Prefix Delegation and used a number of /64s from this larger prefix.</p>
+                    </div>
+                <div class="modal-footer card-opacity-cv">
+                    <a href="#!" class="modal-action modal-close btn blue darken-4 white-text">Close</a>
+                </div>
+            </div>
+						&nbsp; &nbsp;
+			<a href="#skills-modal-test11" class="btn modal-trigger waves-effect waves-light card-opacity-cv" id ="">VMware ESXi</a>
+				
+			   <div id="skills-modal-test11" class="modal card-opacity-cv">
+                    <div class="modal-content card-opacity-cv white-text">
+                        <h4>VMware vSphere - ESXi and vCenter</h4>
+                        <p>Experemented with ESXi and vCenter (5.5, 6.5, 6.7) in a small lab environment consisting of a three host cluster with a number of VMs</p>
+                    </div>
+                <div class="modal-footer card-opacity-cv">
+                    <a href="#!" class="modal-action modal-close btn blue darken-4 white-text">Close</a>
+                </div>
+            </div>
+						&nbsp; &nbsp;
+			<a href="#skills-modal-test12" class="btn modal-trigger waves-effect waves-light card-opacity-cv" id ="">HTML 5</a>
+				
+			   <div id="skills-modal-test12" class="modal card-opacity-cv">
+                    <div class="modal-content card-opacity-cv white-text">
+                        <h4>Hypertext Markup Language</h4>
+                        <p>Learned HTML5 in the first year of my university studies, completing a number of projects - mostly available on github.</p>
+                    </div>
+                <div class="modal-footer card-opacity-cv">
+                    <a href="#!" class="modal-action modal-close btn blue darken-4 white-text">Close</a>
+                </div>
+            </div>
+						&nbsp; &nbsp;
+			<a href="#skills-modal-test13" class="btn modal-trigger waves-effect waves-light card-opacity-cv" id ="">CSS3</a>
+				
+			   <div id="skills-modal-test13" class="modal card-opacity-cv">
+                    <div class="modal-content card-opacity-cv white-text">
+                        <h4>Cascading Style Sheets</h4>
+                        <p>First year university projects - mostly available publicly on github</p>
+                    </div>
+                <div class="modal-footer card-opacity-cv">
+                    <a href="#!" class="modal-action modal-close btn blue darken-4 white-text">Close</a>
+                </div>
+            </div>
+						&nbsp; &nbsp;
+			<a href="#skills-modal-test14" class="btn modal-trigger waves-effect waves-light card-opacity-cv" id ="">Git</a>
+				
+			   <div id="skills-modal-test14" class="modal card-opacity-cv">
+                    <div class="modal-content card-opacity-cv white-text">
+                        <h4>Version Control Systems - GIT</h4>
+                        <p>Frequent utilisation of the Git version control system with university projects, both via CLI and a VSCode plugin, mostly basic use but with some branching.</p>
+                    </div>
+                <div class="modal-footer card-opacity-cv">
+                    <a href="#!" class="modal-action modal-close btn blue darken-4 white-text">Close</a>
+                </div>
+            </div>
+						&nbsp; &nbsp;
+			<a href="#skills-modal-test15" class="btn modal-trigger waves-effect waves-light card-opacity-cv" id ="">GitHub</a>
+				
+			   <div id="skills-modal-test15" class="modal card-opacity-cv">
+                    <div class="modal-content card-opacity-cv white-text">
+                        <h4>GitHub</h4>
+                        <p>Frequent use as part of University projects, including use of branches, pull requests and merging, and the issue tracking system.</p>
+                    </div>
+                <div class="modal-footer card-opacity-cv">
+                    <a href="#!" class="modal-action modal-close btn blue darken-4 white-text">Close</a>
+                </div>
+            </div>
+						&nbsp; &nbsp;
+			<a href="#skills-modal-test16" class="btn modal-trigger waves-effect waves-light card-opacity-cv" id ="">Cisco Autonomous AP's</a>
+				
+			   <div id="skills-modal-test16" class="modal card-opacity-cv">
+                    <div class="modal-content card-opacity-cv white-text">
+                        <h4>Cisco Wireless Access Points - 1141N</h4>
+                        <p>Configured an 1141N to use a RADIUS server to authenticate clients on a homelab SSID - this required a basic internal Certificate Authority and configuration of a FreeRADIUS server to pass authentication requests to Microsoft Active Directory. Users would authenticate using their AD Username and Password.</p>
+                    </div>
+                <div class="modal-footer card-opacity-cv">
                     <a href="#!" class="modal-action modal-close btn blue darken-4 white-text">Close</a>
                 </div>
             </div>
