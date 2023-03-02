1178be294e80b61fc4402fab961f54b2fb85f1cb
tireland1985 <tim.ireland19@my.northampton.ac.uk>
2023-03-02 02:59:23 +0000

    moving modals specific jQuery to dedicated file

---
+++ httpdocs/scripts/modals.js
@@ -0,0 +1,11 @@
+//materialize jQuery to load modals
+$(document).ready(function(){
+	//initialise all modals
+	$('.modal').modal();
+			
+	//open modal automatically
+	//$('#skills-modal-test').modal('open');
+	
+	//load modal on click
+	$('.trigger-modal').modal();
+  });
