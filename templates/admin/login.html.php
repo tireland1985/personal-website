4f66671533352004e65d1d73340cae22111f32d9
tireland1985 <tim.ireland19@my.northampton.ac.uk>
2023-03-07 01:02:19 +0000

    styling login form

--- templates/admin/login.html.php
+++ templates/admin/login.html.php
@@ -1,4 +1,4 @@
-<main id="admin-page">
+<main id="admin-login">
     <?php if(isset($error)): ?>
         <div class="errors">
             &emsp; <strong><?=$error;?> </strong>
@@ -10,28 +10,32 @@
                 <div class="info">
                     <h3>Log In:</h3>
                 </div>
-                <div class="row contact-me">
+                <div class="row login-me">
                     <div class="card horizontal card-opacity center">
                         <div class="card-stacked">
                             <form onsubmit="return(validateForm());" action="/login/auth" method="post" class="col s8 m8 center" id="login-form" name="loginForm">
                                 <div class="row col s12">
-                                    <div class="input-field col s6 center">
+                                    <div class="input-field col s8 center">
                                         <i class="material-icons prefix">account_circle</i>
                                         <input type="email" id="username" class="validate white-text" name="username">
                                         <label for="username" class="white-text" data-error="wrong" data-success="right">Email</label>
 
                                     </div>
                                 </div>
-                                <div class="row">
-                                    <div class="input-field col s6 center">
+                                <div class="row col s12">
+                                    <div class="input-field col s8 center">
                                         <i class="material-icons prefix">key</i>
                                         <input type="password" class="white-text" id="password" name="password">
                                         <label for="password" class="white-text">Password</label>
 
                                     </div>
                                 </div>
-                                <input type="hidden" name="login_token" value="<?= $_SESSION['login_token'] ?? '' // basic CSRF protection ?>"> 
-                                <button type="submit" name="submit">Log In &nbsp;<i class="material-icons white-text">send</i></button>
+                                <div class="row col s8 m8 center">
+                                    <input type="hidden" name="login_token" value="<?= $_SESSION['login_token'] ?? '' // basic CSRF protection  ?>"> 
+                                    <button type="submit" name="submit">Log In &nbsp;<i class="material-icons white-text">send</i></button>
+                                </div>
+                                    
+                               
                             </form>
                         </div>
                     </div>
