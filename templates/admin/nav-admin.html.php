1615d62cc57e8c6d3f8b493ad744731eff1840c2
tireland1985 <tim.ireland19@my.northampton.ac.uk>
2023-03-11 20:37:06 +0000

    add links to admin pages

--- templates/admin/nav-admin.html.php
+++ templates/admin/nav-admin.html.php
@@ -1,18 +1,19 @@
 <li class="admin-nav">Portfolio:</li>
-<li class="admin-nav"><a href="">Personal Projects</a></li>
-<li class="admin-nav"><a href="">University Projects</a></li>
-<li class="admin-nav"><a href="">test3</a></li>
-<li class="admin-nav"><a href="">test4</a></li>
-<li class="admin-nav"><a href="">test5</a></li>
+<li class="admin-nav"><a href="/portfolio/showPersonal">Personal Projects</a></li>
+<li class="admin-nav"><a href="/portfolio/showUniversity">University Projects</a></li>
+<li class="admin-nav"><a href="/portfolio/showProjects">All Projects</a></li>
+
 <br />
 <li class="admin-nav">CV:</li>
-<li class="admin-nav"><a href="">Professional Experience</a></li>
-<li class="admin-nav"><a href="">Other Experience</a></li>
-<li class="admin-nav"><a href="">Skills</a></li>
-<li class="admin-nav"><a href="">Education</a></li>
-<li class="admin-nav"><a href="">test5</a></li>
+<li class="admin-nav"><a href="/cv/showProfessionalExperience">Professional Experience</a></li>
+<li class="admin-nav"><a href="/cv/showOtherExp">Other Experience</a></li>
+<li class="admin-nav"><a href="/cv/showSkills">Skills</a></li>
+<li class="admin-nav"><a href="/cv/showEducation">Education</a></li>
+
 <br />
 <li class="admin-nav">User:</li>
-<li class="admin-nav"><a href="">Profile</a></li>
-<li class="admin-nav"><a href="">List</a></li>
-<li class="admin-nav"><a href="">Logout</a></li>
+<li class="admin-nav"><a href="/user/profile">Profile</a></li>
+<li class="admin-nav"><a href="/user/showUsers">List</a></li>
+<li class="admin-nav"><a href="/user/loginAttempts">Login Attempts</a></li>
+<li class="admin-nav"><a href="/user/register">Registration</a></li>
+<li class="admin-nav"><a href="/login/logout">Logout</a></li>
