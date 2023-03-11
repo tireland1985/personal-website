817ab491157c4bc9c82c90995c01439ae62a172e
tireland1985 <tim.ireland19@my.northampton.ac.uk>
2023-03-11 03:00:30 +0000

    add login attempts table to admin controller, include query to show last successful login

--- PortfolioSite/Controllers/adminController.php
+++ PortfolioSite/Controllers/adminController.php
@@ -5,17 +5,18 @@ class adminController{
 
     private $userTable;
 
-    public function __construct($authentication, $userTable){
+    public function __construct($authentication, $userTable, $loginTable){
         $this->authentication = $authentication;
         $this->userTable = $userTable;
+        $this->loginTable = $loginTable;
     }
 
     public function home(){
-
+        $lastLoginSuccess = $this->loginTable->findTwoLatestOnly('email', $_SESSION['username'], 'status', 'SUCCESS', 'datetime_attempted');
         return [
             'template' => 'admin/home.html.php',
             'title' => 'Administration Area',
-            'variables' => []
+            'variables' => ['lastLoginSuccess' => $lastLoginSuccess]
         ];
     }
 }
