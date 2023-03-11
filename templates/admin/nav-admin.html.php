c2bda7ea3a17437152d03035006161e808cd2a2a
tireland1985 <tim.ireland19@my.northampton.ac.uk>
2023-03-11 03:01:11 +0000

    update layout of admin menu

--- templates/admin/nav-admin.html.php
+++ templates/admin/nav-admin.html.php
@@ -1,5 +1,18 @@
-<li class="admin-nav"><a href="">test1</a></li>
-<li class="admin-nav"><a href="">test2</a></li>
+<li class="admin-nav">Portfolio:</li>
+<li class="admin-nav"><a href="">Personal Projects</a></li>
+<li class="admin-nav"><a href="">University Projects</a></li>
 <li class="admin-nav"><a href="">test3</a></li>
 <li class="admin-nav"><a href="">test4</a></li>
 <li class="admin-nav"><a href="">test5</a></li>
+<br />
+<li class="admin-nav">CV:</li>
+<li class="admin-nav"><a href="">Professional Experience</a></li>
+<li class="admin-nav"><a href="">Other Experience</a></li>
+<li class="admin-nav"><a href="">Skills</a></li>
+<li class="admin-nav"><a href="">Education</a></li>
+<li class="admin-nav"><a href="">test5</a></li>
+<br />
+<li class="admin-nav">User:</li>
+<li class="admin-nav"><a href="">Profile</a></li>
+<li class="admin-nav"><a href="">List</a></li>
+<li class="admin-nav"><a href="">Logout</a></li>
