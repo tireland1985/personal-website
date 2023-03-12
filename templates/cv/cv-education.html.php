2ec2c36221bf2053ad2b8e1721c205862d748644
tireland1985 <tim.ireland19@my.northampton.ac.uk>
2023-03-12 03:18:44 +0000

    add cv templates for edication & employment pages

--- templates/cv/cv-education.html.php
+++ templates/cv/cv-education.html.php
@@ -1,3 +1,20 @@
 <main id="cv-page">
-
+<div class="row">
+    <div class="col s12 m12 center white-text">
+        <h2 class="header">Education</h2>
+        <div class="cv-container">
+            <?php foreach($educationList as $row){ ?>
+                <div class="card horizontal center card-opacity-cv">
+                    <div class="card-stacked">
+                        <div class="card-content">
+                            <h3><a href="<?=$row->institute_url ?? ''?>"><?=$row->institute_name ?? '' ?></a></h3>
+                            <h4><?=$row->start_year ?? ''?> - <?=$row->end_year ?? '' ?></h4>
+                            <p class="cv-text"><?=$row->course_names ?? '' ?></p>
+                        </div>
+                    </div>
+                </div>
+          <?php  } ?>
+            </div>
+    </div>
+</div>
 </main>
--- templates/cv/cv-pro-exp.html.php
+++ templates/cv/cv-pro-exp.html.php
@@ -1,3 +1,25 @@
 <main id="cv-page">
-
+    <div class="cv-container">
+        <h2 class="header">Professional Experience</h2>
+        <div class="row">
+            <div class="col s12 m12 center">
+                <?php foreach($employmentList as $row){ ?>
+                    <div class="card horizontal center card-opacity-cv white-text">
+                        <div class="card-stacked">
+                            <div class="card-content">
+                                <h4><a href="<?=$row->employer_url ?? '' ?>"><?=$row->employer_name ?? '' ?></a></h4>
+                                <?php if(!empty($row->start_date) && !empty($row->end_date)){ ?>
+                                <p class="cv-text"><?=$row->start_date ?? '' ?> - <?=$row->end_date ?? '' ?></p>
+                                <?php } 
+                                if(!empty($row->start_date) && empty($row->end_date)){ ?>
+                                    <p class="cv-text"><?=$row->start_date ?? ''?> - present</p>
+                                <?php } ?>
+                                <p class="cv-text"><?=$row->duties ?? '' ?></p>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
 </main>
