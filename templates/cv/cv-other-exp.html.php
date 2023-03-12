575a049a51675345bd04be5e860e533e94b7fb3a
tireland1985 <tim.ireland19@my.northampton.ac.uk>
2023-03-12 03:56:48 +0000

    change template layout

--- templates/cv/cv-other-exp.html.php
+++ templates/cv/cv-other-exp.html.php
@@ -1,17 +1,25 @@
 <main id="cv-page">
     <div class="cv-container">
+        <div class="row col s12 m12 center">
+            <h2 class="header">Other Experience</h2>
+            <img src="https://timireland.uk/images/homelab_1.jpg" alt="homelab">
+        </div>
+    </div>
+    <?php foreach($otherExperienceList as $row){ ?>
+        <div class="cv-container">
         <div class="row">
             <div class="col s12 m12 center">
-                <h2 class="header">Other Experience</h2>
-                <img src="https://timireland.uk/images/homelab_1.jpg" alt="homelab picture" class="profile center">
-                    <?php foreach($otherExperienceList as $row){ ?>
-                        <div class="card horizontal card-opacity-cv white-text">
+                <div class="card horizontal card-opacity-cv center white-text">
+                    <div class="card-stacked">
+                        <div class="card-content">
                             <h4><?=$row->title ?? ''?></h4>
+                            <br />
                             <?=$row->details ?? '' ?>
                         </div>
-
-                    <?php } ?>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+    <?php } ?>
 </main>
