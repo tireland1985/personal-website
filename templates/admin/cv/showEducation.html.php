d1d17e7c4231370baf208301a7629951224d848d
tireland1985 <tim.ireland19@my.northampton.ac.uk>
2023-03-15 03:53:16 +0000

    add administrative view template for education

---
+++ templates/admin/cv/showEducation.html.php
@@ -0,0 +1,35 @@
+<main id="admin-page">
+<div class="row">
+        <div class="col s3">
+        <ul>
+            <?php if(isset($_SESSION['user_role']) && $_SESSION['user_role'] == '1'){
+                require_once('../templates/admin/nav-admin.html.php');
+            } ?>
+        </ul>
+        </div>
+        <div class="col s9">
+            <!-- CONTENT HERE-->
+            <div>
+                <a href="/cv/editEducation">Add Education</a>
+            </div>
+            <table>
+                <tr>
+                    <th>Educational Institute</th> <th>Start Year</th> <th>Graduation Year</th>
+                </tr>
+                <tr>
+                    <?php foreach($educationList as $row){ ?>
+                        <td><?=$row->institute_name;?></td>
+                        <td><?=$row->start_year;?></td>
+                        <td><?=$row->end_year ?? '' ?></td>
+                        <td><a href="/cv/editEducation?id=<?=$row->id;?>">Edit</a></td>
+                        <td>
+                            <form action="/cv/deleteEducation" method="post">
+                                <input type="hidden" name="id" value="<?=$row->id;?>" />
+                                <input type="submit" value="Delete">
+                            </form>
+                        </td>
+                    <?php } ?>
+                </tr>
+            </table>
+        </div>
+</main>
