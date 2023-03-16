0318df1a192703a98ac6e68e6b99707ff98a083f
tireland1985 <tim.ireland19@my.northampton.ac.uk>
2023-03-16 04:04:45 +0000

    flesh out userController

--- PortfolioSite/Controllers/userController.php
+++ PortfolioSite/Controllers/userController.php
@@ -9,12 +9,138 @@ class userController{
         $this->post = $post;
     }
 
-    public function profile(){
+    public function profile($errors = []){
+        if(isset($_SESSION['user_id'])){
+            $result = $this->userTable->find('id', $_SESSION['user_id']);
+            $currentUser = $result[0];
+            $user = [];
+
+            return [
+                'template' => 'user_profile.html.php',
+                'title' => '',
+                'variables' => ['currentUser' => $currentUser, 'errors' => $errors]
+            ];
+        }
+        else {
+            //user isn't logged in
+            header('location: /login/auth');
+        }
+
+    }
+
+    public function profileSubmit(){
+        $user = $this->post['user'];
+        $currentPass = $user['password'];
+        $newPass = $user['new_password'];
+        $newPassConfirm = $user['confirm_new_password'];
+        $result = $this->userTable->find('id', $_SESSION['user_id']);
+        $userResult = $result[0];
+
+        $valid = true;
+        $errors = [];
+
+        if(empty($currentPass)){
+            $valid = false;
+            $errors[] = 'Enter your existing password';
+        }
+        if(empty($newPass) || empty($newPassConfirm)){
+            $valid = false;
+            $errors[] = 'New/ Confirm password fields are required';
+        }
+        if($newPass !== $newPassConfirm){
+            $valid = false;
+            $errors[] = 'New and Confirm Password values must match';
+        }
+
+        if($valid == true){
+            //verify that the user provided the correct password
+            if(password_verify($currentPass, $userResult->password)){
+                $user['password'] = password_hash($newPass, PASSWORD_DEFAULT);
+                unset($user['new_password']);
+                unset($user['confirm_new_password']);
+
+                $this->userTable->save($user);
+
+                header('location: /admin/home');
+            }
+            else {
+                $valid = false;
+                $errors[] = 'Incorrect Password';
+            }
+        }
+    }
+    public function showUsers(){
+        $userList = $this->userTable->findAll();
 
         return [
-            'template' => 'user_profile.html.php',
-            'title' => '',
-            'variables' => []
+            'template' => 'admin/user/showUsers.html.php',
+            'title' => 'Admin - Users',
+            'variables' => ['userList' => $userList]
         ];
+    }
+
+    public function editUsers($errors = []){
+        $userList = $this->userTable->findAll();
+
+        if(isset($this->get['id'])){
+            $result = $this->userTable->find('id', $this->get['id']);
+            $item = $result[0];
+        }
+        else {
+            $item = false;
+        }
+        return [
+            'template' => 'admin/user/editUsers.html.php',
+            'title' => 'Admin - Edit Users',
+            'variables' => [
+                'item' => $item,
+                'userList' => $userList,
+                'errors' => $errors
+            ]
+        ];
+    }
+
+    public function editUsersSubmit(){
+        $user = $this->post['user'];
+
+        $valid = true;
+        $errors = [];
+
+        if(empty($user['firstname'])){
+            $valid = false;
+            $errors[] = 'Firstname is required';
+        }
+        if(empty($user['lastname'])){
+            $valid = false;
+            $errors[] = 'Lastname is required';
+        }
+        if(empty($user['email'])){
+            $valid = false;
+            $errors[] = 'Email must be provided';
+        }
+        if(filter_var($user['email'], FILTER_VALIDATE_EMAIL) == false){
+            $valid = false;
+            $errors[] = 'Email address must be valid';
+        }
+        if($user['id'] == 1){
+            $valid = false;
+            $errors[] = 'Specified User cannot be modified';
+        }
+        if($valid == true){
+            $this->userTable->save($user);
+            header('location: /user/showUsers');
+        }
+
+    }
+
+    public function deleteSubmit(){
+        //TODO: check privileges
+        if($this->post['id'] !== 1){
+            $this->userTable->delete($this->post['id']);
+            header('location: /user/showUsers');
+        }
+        else {
+            //cannot delete user id 1 - redirect to error page
+        }
     }
 }
