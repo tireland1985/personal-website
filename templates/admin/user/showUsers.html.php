2f88c370c7eae9dbb59fd1cffb7affc72b9219d0
tireland1985 <tim.ireland19@my.northampton.ac.uk>
2023-03-16 04:05:00 +0000

    add admin view for users

---
+++ templates/admin/user/showUsers.html.php
@@ -0,0 +1,36 @@
+<main id="admin-page">
+<div class="row">
+        <div class="col s3">
+        <ul>
+            <?php if(isset($_SESSION['user_role']) && $_SESSION['user_role'] == '1'){
+                require_once('../templates/admin/nav-admin.html.php');
+            } ?>
+        </ul>
+        </div>
+
+        <div class="col s9">
+            <!-- CONTENT HERE -->
+            <table>
+                <tr>
+                    <th>Email</th>
+                    <th>Firstname</th>
+                    <th>Lastname</th>
+                </tr>
+                <?php foreach($userList as $row){ ?>
+                    <tr>
+                        <td><?=$row->email;?></td>
+                        <td><?=$row->firstname;?></td>
+                        <td><?=$row->lastname;?></td>
+                        <td><a href="/user/editUser?id=<?=$row->id;?>">Edit</a></td>
+                        <td>
+                            <form action="/user/deleteUser" method="post">
+                                <input type="hidden" name="id" value="<?=$row->id;?>" />
+                                <input type="submit" value="Delete" />
+                            </form>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </table>
+        </div>
+</div>
+</main>
