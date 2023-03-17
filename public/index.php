53ac809ba6cae11d9b9e45ec1340c78bc480b799
tireland1985 <tim.ireland19@my.northampton.ac.uk>
2023-03-17 05:00:43 +0000

    move HTMLPurifier code to Routes.php

--- public/index.php
+++ public/index.php
@@ -5,14 +5,6 @@ require_once '../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '../.env.dev'); // define, then load the specified .env file
 $dotenv->load();
 
-//initialise HTMLPurifier (testing)
-require_once '../vendor/ezyang/htmlpurifier/library/HTMLPurifier.auto.php';
-$config = HTMLPurifier_HTML5Config::createDefault();
-//$config = \HTMLPurifier_Config::createDefault();
-//Allow only basic tags
-$config->set('HTML.Allowed', 'p,b,a[href],i, ul, li');
-$purifier = new \HTMLPurifier($config);
-
 $routes = new \PortfolioSite\Routes();
 $entryPoint = new \Classes\EntryPoint($routes);
 $entryPoint->run();
