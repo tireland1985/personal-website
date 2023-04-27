07a91e957bcf25bf9da11e752cd0b2d673cef840
tireland1985 <tim.ireland19@my.northampton.ac.uk>
2023-04-27 03:42:35 +0100

    flesh out show and edit functions

--- PortfolioSite/Controllers/certsController.php
+++ PortfolioSite/Controllers/certsController.php
@@ -11,14 +11,52 @@ class certsController{
     }
 
     public function show(){
-
+        // admin function - display records
+        $certsList = $this->certsTable->findAll(); 
+        return [
+           'template' => 'admin/certs/showCerts.html.php',
+           'title' => 'Admin - Certificates',
+           'variables' => ['certsList' => $certsList]
+           ];
     }
 
     public function edit($errors = []){
+        // admin function - edit records
+        $certsList = $this->certsTable->findAll();
 
+        if(isset($this->get['id'])){
+            $result = $this->certsTable->find('id', $this->get['id']);
+            $item = $result[0];
+        }
+        else {
+            $item = false;
+        }
+
+        return [
+            'template' => 'admin/certs/editCerts.html.php',
+            'title' => 'Admin - Edit Certiificates',
+            'variables' => ['item' => $item, 'certsList' => $certsList, 'errors' => $errors]
+        ];
     }
 
     public function editSubmit(){
-        
+        $cert = $this->post['cert'];
+        $errors = [];
+        $valid = true;
+
+        // validation checks
+
+        //submission is valid
+        if($valid == true){
+            //Purify submission
+            // ..
+            // ..
+            // ..
+            // ..
+            // ..
+            //save data
+            $this->certsTable->save($cert);
+            header('location: /certs/show');
+        }
     }
 }
